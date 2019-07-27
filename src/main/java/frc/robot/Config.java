bdcc2f3052fd1803f9ffb7f3ec994e9e3d9a561d
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-07-27 00:04:08 -0400

    Added all the smartdashbaord keys

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -34,4 +34,40 @@ public class Config {
 
     public static final int VACUUM = 4;
   }
+
+
+  public static final class SmartDashboardKeys {
+
+
+    public static class UserEditable {
+
+      public static final String LINEAR_ACTUATOR_MOVE_DOWN_TIMEOUT = "UserEditable/Linear Actuator Move Down Timeout";
+      public static final String LINEAR_ACTUATOR_MOVE_UP_TIMEOUT = "UserEditable/Linear Actuator Move Up Timeout";
+      public static final String LINEAR_ACTUATOR = "UserEditable/Linear Actuator Speed";
+      public static final String TOTAL_ROTATION_TIME = "UserEditable/Total Rotation Time";
+      public static final String TOTAL_VIBRATION_TIME = "UserEditable/Total Vibration Time";
+      public static final String TURRET_START_POSITION = "UserEditable/Turret Start Position";
+      public static final String TURRET_END_POSITION = "UserEditable/Turret End Position";
+      public static final String TOLERANCE = "UserEditable/Tolerance";
+      public static final String ARM_START_POSITION = "UserEditable/Arm Start Position";
+      public static final String ARM_END_POSITION = "UserEditable/Arm End Position";
+      public static final String ROTATE_CAN_180_DEGREES = "UserEditable/Rotate Can 180 Degrees";
+      public static final String ROTATE_CAN_0_DEGREES = "UserEditable/Rotate CAN 0 Degrees";
+
+    }
+
+    public static class Hardware {
+
+      public static final String TURN_TABLE_POWER = "Hardware/TurnTable Power";
+      public static final String TURN_TABLE_POSITION = "Hardware/TurnTable Position";
+      public static final String LINEAR_ACTUATOR_POWER = "Hardware/Linear Actuator Power";
+      public static final String CAN_SPINNER_POWER = "Hardware/CanSpinner Power";
+      public static final String CAN_SPINNER_POSITION = "Hardware/CanSpinner Position";
+      public static final String ARM_MOTOR_POWER = "Hardware/ArmMotor Power";
+      public static final String ARM_MOTOR_POSITION = "Hardware/ArmMotor Position";
+      public static final String MOTOR_VIBRATOR = "Hardware/MotorVibrator";
+      public static final String VACUUM = "Hardware/Vacuum";
+
+    }
+  }
 }

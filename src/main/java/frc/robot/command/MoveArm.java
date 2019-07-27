fbaa58b661f5090553ba3d7ab0169e58cca7680b
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-07-27 00:07:58 -0400

    Uses CTRE position controller to move to target within tolerance

--- src/main/java/frc/robot/command/MoveArm.java
+++ src/main/java/frc/robot/command/MoveArm.java
@@ -1,30 +1,39 @@
 package frc.robot.command;
 
+import static frc.robot.Config.SmartDashboardKeys.UserEditable.TOLERANCE;
+import static frc.robot.Hardware.turnTable;
 import static frc.robot.Robot.GOD_SUBSYSTEM;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * @author Marius Juston
  **/
 public class MoveArm extends Command {
 
-  public MoveArm() {
+  private final int target;
+
+  public MoveArm(int target) {
     super(GOD_SUBSYSTEM);
+    this.target = target;
   }
 
   @Override
   protected void initialize() {
-    System.out.println("Moving Arm");
+    turnTable.set(ControlMode.Position, target);
+    System.out.printf("Moving Arm to %d%n", target);
   }
 
   @Override
   protected void end() {
-    System.out.println("Stop Moving Arm");
+    turnTable.set(ControlMode.PercentOutput, 0);
+    System.out.printf("Stopping Arm. Arm Stopped at %d%n", turnTable.getSelectedSensorPosition());
   }
 
   @Override
   protected boolean isFinished() {
-    return false;
+    return Math.abs(turnTable.getSelectedSensorPosition() - target) <= SmartDashboard.getNumber(TOLERANCE, 50);
   }
 }
--- src/main/java/frc/robot/command/MoveTurret.java
+++ src/main/java/frc/robot/command/MoveTurret.java
@@ -1,20 +1,39 @@
 package frc.robot.command;
 
+import static frc.robot.Config.SmartDashboardKeys.UserEditable.TOLERANCE;
+import static frc.robot.Hardware.turnTable;
 import static frc.robot.Robot.GOD_SUBSYSTEM;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * @author Marius Juston
  **/
 public class MoveTurret extends Command {
 
-  public MoveTurret() {
+  private final int target;
+
+  public MoveTurret(int target) {
     super(GOD_SUBSYSTEM);
+    this.target = target;
+  }
+
+  @Override
+  protected void initialize() {
+    turnTable.set(ControlMode.Position, target);
+    System.out.printf("Moving Turret to %d%n", target);
+  }
+
+  @Override
+  protected void end() {
+    turnTable.set(ControlMode.PercentOutput, 0);
+    System.out.printf("Stopping Turret. Turret Stopped at %d%n", turnTable.getSelectedSensorPosition());
   }
 
   @Override
   protected boolean isFinished() {
-    return false;
+    return Math.abs(turnTable.getSelectedSensorPosition() - target) <= SmartDashboard.getNumber(TOLERANCE, 50);
   }
 }

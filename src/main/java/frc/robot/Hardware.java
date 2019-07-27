edfcf81d296aaee7157155d8f5ff402981fbd1c9
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-07-27 00:03:52 -0400

    Setup the CAN talon devices

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -7,6 +7,9 @@ import static frc.robot.Config.CanID.MOTOR_VIBRATOR;
 import static frc.robot.Config.CanID.TURN_TABLE;
 import static frc.robot.Config.PCM.VACUUM;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrame;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.Solenoid;
 
@@ -15,7 +18,7 @@ import edu.wpi.first.wpilibj.Solenoid;
  **/
 public class Hardware {
 
-  public static final TalonSRX turntable = new TalonSRX(TURN_TABLE);
+  public static final TalonSRX turnTable = new TalonSRX(TURN_TABLE);
   public static final TalonSRX linearActuator = new TalonSRX(LINEAR_ACTUATOR);
   public static final TalonSRX canSpinner = new TalonSRX(CAN_SPINNER);
   public static final TalonSRX armMotor = new TalonSRX(ARM_MOTOR);
@@ -24,7 +27,60 @@ public class Hardware {
 
 
   public static void init() {
+    turnTable.setNeutralMode(NeutralMode.Brake);
+    linearActuator.setNeutralMode(NeutralMode.Brake);
+    canSpinner.setNeutralMode(NeutralMode.Brake);
+    armMotor.setNeutralMode(NeutralMode.Brake);
+    motorVibrator.setNeutralMode(NeutralMode.Brake);
+
+    turnTable.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+    armMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+    canSpinner.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+
+//    turnTable.setSensorPhase(false);
+//    turnTable.setInverted(false);
+//    armMotor.setSensorPhase(false);
+//    armMotor.setInverted(false);
+//    canSpinner.setSensorPhase(false);
+//    canSpinner.setInverted(false);
+
+    turnTable.setStatusFramePeriod(StatusFrame.Status_10_Targets, 10, 0);
+    armMotor.setStatusFramePeriod(StatusFrame.Status_10_Targets, 10, 0);
+    canSpinner.setStatusFramePeriod(StatusFrame.Status_1_General, 10, 0);
+
+    turnTable.configPeakOutputForward(1);
+    turnTable.configPeakOutputReverse(1);
+
+    linearActuator.configPeakOutputForward(1);
+    linearActuator.configPeakOutputReverse(1);
+
+    canSpinner.configPeakOutputForward(1);
+    canSpinner.configPeakOutputReverse(1);
+
+    armMotor.configPeakOutputForward(1);
+    armMotor.configPeakOutputReverse(1);
+
+    motorVibrator.configPeakOutputForward(1);
+    motorVibrator.configPeakOutputReverse(1);
+
+    turnTable.selectProfileSlot(0, 0);
+    armMotor.selectProfileSlot(0, 0);
+    canSpinner.selectProfileSlot(0, 0);
+
+//    turnTable.configForwardSoftLimitThreshold();
+//    turnTable.configReverseSoftLimitThreshold();
+    turnTable.configReverseSoftLimitEnable(false, 10);
+    turnTable.configForwardSoftLimitEnable(false, 10);
+//
+//    armMotor.configForwardSoftLimitThreshold();
+//    armMotor.configReverseSoftLimitThreshold();
+    armMotor.configReverseSoftLimitEnable(false, 10);
+    armMotor.configForwardSoftLimitEnable(false, 10);
+//
+//    canSpinner.configForwardSoftLimitThreshold();
+//    canSpinner.configReverseSoftLimitThreshold();
+    canSpinner.configReverseSoftLimitEnable(false, 10);
+    canSpinner.configForwardSoftLimitEnable(false, 10);
 
   }
-
 }
